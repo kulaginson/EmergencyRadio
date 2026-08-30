001998b39db8fd256cfd5805a9b13d3a1e220b2b
kulaginson <kulaginson@gmail.com>
2026-08-30 20:22:17 +0400

    Update dllmain.cpp

--- dllmain.cpp
+++ dllmain.cpp
@@ -1,1 +1,65 @@
+#include <windows.h>
 
+#include <plugin.h>
+
+#include "CVehicle.h"
+#include "CPed.h"
+#include "CPlayerPed.h"
+#include "CWorld.h"
+
+namespace
+{
+    constexpr int RADIO_CIVILIAN = 0;
+
+    void EnableEmergencyVehicleRadio()
+    {
+        CPlayerPed* player = FindPlayerPed(-1);
+
+        if (!player)
+            return;
+
+        CVehicle* vehicle = player->m_pVehicle;
+
+        if (!vehicle)
+            return;
+
+        const bool emergency =
+            vehicle->bIsLawEnforcer ||
+            vehicle->bIsAmbulanceOnDuty ||
+            vehicle->bIsFireTruckOnDuty;
+
+        if (!emergency)
+            return;
+
+        vehicle->m_vehicleAudio.m_settings.m_nRadioType =
+            RADIO_CIVILIAN;
+    }
+
+    class EmergencyRadio
+    {
+    public:
+        EmergencyRadio()
+        {
+            plugin::Events::gameProcessEvent += []()
+            {
+                EnableEmergencyVehicleRadio();
+            };
+        }
+    };
+
+    EmergencyRadio g_emergencyRadio;
+}
+
+BOOL APIENTRY DllMain(
+    HMODULE hModule,
+    DWORD reason,
+    LPVOID
+)
+{
+    if (reason == DLL_PROCESS_ATTACH)
+    {
+        DisableThreadLibraryCalls(hModule);
+    }
+
+    return TRUE;
+}
